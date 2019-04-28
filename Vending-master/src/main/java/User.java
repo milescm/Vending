5577edf2e863d51649d58894a8d4b70660a871a0
hongpoleon <milescm@nate.com>
2019-04-28 17:43:15 +0900

    Add User Class

--- Vending-master/src/main/java/User.java
+++ Vending-master/src/main/java/User.java
@@ -1,2 +1,18 @@
 public class User {
+
+    private int money;
+
+    public int getMoney(){
+        return this.money;
+    }
+
+    public void inputMoney(int input){
+        money += input;
+    }
+    public void change(int change){
+        if(change>money){
+            throw new IllegalArgumentException();
+        }
+        money -= change;
+    }
 }
