8e0554758845267b7f4f34aee1351957c2d59b41
hongpoleon <milescm@nate.com>
2019-04-28 17:50:46 +0900

    Add Test method in UserTest Class

--- Vending-master/src/test/java/UserTest.java
+++ Vending-master/src/test/java/UserTest.java
@@ -1,7 +1,44 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+@Test
+    public void initUser(){
+    User user = new User();
+    user.getMoney();
+    assertThat(user.getMoney(), is(0));
+}
+
+@Test
+    public void test_insertMoney(){
+    User user = new User();
+
+    user.inputMoney(1000);
+    assertThat(user.getMoney(), is(1000));
+}
+
+@Test
+    public void test_changeMoney(){
+    User user = new User();
+    VendingMachine drink = new VendingMachine();
+    user.inputMoney(2000);
+    assertThat(user.getMoney(), is(2000));
+    user.change(drink.sodar.getPrice());
+    assertThat(user.getMoney(), is(1000));
+}
+
+@Test(expected = IllegalArgumentException.class)
+    public void test_Nomoney(){
+    User user = new User();
+    VendingMachine drink = new VendingMachine();
+    user.inputMoney(500);
+    user.change(drink.coke.getPrice());
+    assertThat(user.getMoney(), is(-500));
+}
 
 
 }
