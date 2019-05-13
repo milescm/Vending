ddbb764efad994f9f59e9ee194ab6279e310a1e2
hongpoleon <milescm@nate.com>
2019-05-13 18:58:28 +0900

    Add getNoDrink Method()

---
+++ Vending/Vending-master/src/main/java/VendingMachine.java
@@ -0,0 +1,17 @@
+public class VendingMachine {
+
+    Drink coke = new Drink("Coke" , 1000);
+    Drink sodar = new Drink("Sodar" , 1000);
+    Drink coffee = new Drink("Coffee" , 1500);
+    Drink water = new Drink("Water" , 500);
+    Drink noDrink = new Drink("noDrink" , -1);
+
+
+
+    public int exchange_money(int money){
+        return money;
+
+    }
+
+
+}
